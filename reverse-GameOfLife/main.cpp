75954d73ac6ed2fdcef0bb4ee8bdfa20b927a964
alimoammeri79 <moammeriali@gmail.com>
2022-02-20 10:51:48 +0330

    Use empty method to check container is empty instead of size method

--- reverse-GameOfLife/main.cpp
+++ reverse-GameOfLife/main.cpp
@@ -72,18 +72,18 @@ bool game_of_life(std::vector<std::string>& world)
             }
             else
             {
-                if((adjacent_mushrooms_count == 3))
+                if(adjacent_mushrooms_count == 3)
                     actions.push_back({MSHRM, Coordinate{i, j}});
             }
         }
-    if(actions.size()) {
+    if(!actions.empty()) {
         do_action(world, actions);
         return true;
     }
     return false;
 }
 
-std::string to_binary(int a, int n)
+std::string to_binary(size_t a, size_t n)
 {
     std::string result(n, DEAD);
     for(std::size_t i{result.length()}; i-- && a;) {
@@ -106,7 +106,7 @@ size_t mushrooms_count(const std::vector<std::string>& world)
 
 
 // I'm trying to keep track of states in order to avoid
-// duplicate moves. But that dosn't work yet.
+// duplicate moves. But that doesn't work yet.
 
 bool extract_state(const std::vector<std::string>& a)
 {
@@ -121,7 +121,8 @@ bool extract_state(const std::vector<std::string>& a)
 }
 **/
 
-std::vector<std::string> find_initial_step(std::vector<std::string>& world, const std::vector<std::string>& goal, int n, int m, int l, int current=0) {
+std::vector<std::string> find_initial_step(std::vector<std::string>& world, const std::vector<std::string>& goal,
+                                           std::size_t n, std::size_t m, std::size_t l, std::size_t current=0) {
 
     if(n == current)
     {
@@ -136,11 +137,11 @@ std::vector<std::string> find_initial_step(std::vector<std::string>& world, cons
 
         return std::vector<std::string>{};
     }
-    for(int k{0}; k < pow(2, m); ++k)
+    for(std::size_t k{0}; k < pow(2, double(m)); ++k)
     {
         world[current] = to_binary(k, m);
         std::vector<std::string> result{find_initial_step(world, goal, n, m, l, current + 1)};
-        if(result.size()){
+        if(!result.empty()){
             return result;
         }
     }
@@ -158,7 +159,7 @@ int main() {
         std::cin >> row;
 
     std::vector<std::string> result{find_initial_step(world, goal, n, m, l)};
-    if(result.size())
+    if(!result.empty())
         for(const auto& row: result)
             std::cout << row << "\n";
     else
