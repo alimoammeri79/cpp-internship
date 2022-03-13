663977579cbc5df7bfc8900aa9cc9d68bac8fc5c
alimoammeri79 <moammeriali@gmail.com>
2022-03-13 14:43:17 +0330

    Use fixture in unit tests

--- reverse-GameOfLife/tests/array2d_test.cc
+++ reverse-GameOfLife/tests/array2d_test.cc
@@ -2,51 +2,48 @@
 #include <cstddef>
 #include "array2d.h"
 
-TEST(Array2dTest, GettersTest) {
-    constexpr std::size_t width {3};
-    constexpr std::size_t hight {3};
+// The fixture for testing class Foo.
+class Array2dTest : public ::testing::Test {
+ protected:
+  Array2dTest() {}
+  void SetUp() override {
+     // Code here will be called immediately after the constructor (right
+     // before each test).
+     m_width = 3;
+     m_hight = 3;
+     m_board = {m_hight, m_width, {1, 2, 3, 4, 5, 6, 7, 8, 9}};
+     m_board_2 = {m_board};
+     m_board_3 = {m_hight, m_width, {1, 2, 3, 4, 5, 6, 0, 8, 9}};
+     m_empty_board = {};
+  }
+  std::size_t m_width;
+  std::size_t m_hight;
+  Array2d<int> m_board;
+  Array2d<int> m_board_2;
+  Array2d<int> m_board_3;
+  Array2d<int> m_empty_board;
+};
 
-    const Array2d<int> my_board {hight, width, {1, 2, 3, 4, 5, 6, 7, 8, 9}};
-    
-    EXPECT_EQ(width, my_board.width());
-    EXPECT_EQ(hight, my_board.hight());
+TEST_F(Array2dTest, GettersTest) {
+    EXPECT_EQ(m_width, m_board.width());
+    EXPECT_EQ(m_hight, m_board.hight());
 }
 
-TEST(Array2dTest, IsEmptyTest) {
-    constexpr std::size_t width {1};
-    constexpr std::size_t hight {1};
-
-    const Array2d<int> my_board {hight, width, {1}};
-    const Array2d<int> my_empty_board;
-    
-    EXPECT_EQ(0, my_board.is_empty());
-    EXPECT_EQ(1, my_empty_board.is_empty());
+TEST_F(Array2dTest, IsEmptyTest) {
+    EXPECT_EQ(false, m_board.is_empty());
+    EXPECT_EQ(true, m_empty_board.is_empty());
 }
 
-TEST(Array2dTest, EqualityTest) {
-    constexpr std::size_t width {2};
-    constexpr std::size_t hight {2};
-
-    const Array2d<int> my_board {hight, width, {1, 2, 6, 8}};
-    const Array2d<int> my_second_board {my_board};
-    const Array2d<int> my_third_board {hight, width, {1, 2, 6, 8}};
-    const Array2d<int> my_forth_board {hight, width, {1, 2, 0, 8}};
-    
-    EXPECT_EQ(1, my_board == my_second_board);
-    EXPECT_EQ(1, my_board == my_third_board);
-    EXPECT_EQ(0, my_board == my_forth_board);
+TEST_F(Array2dTest, EqualityTest) {
+    EXPECT_EQ(true, m_board == m_board_2);
 }
 
-TEST(Array2dTest, AccessWithIndexTest) {
-    constexpr std::size_t width {2};
-    constexpr std::size_t hight {2};
-
-    Array2d<int> my_board {hight, width, {1, 2, 6, 8}};
+TEST_F(Array2dTest, AccessWithIndexTest) {
+        
+    EXPECT_EQ(1, m_board[0][0]);
+    EXPECT_EQ(4, m_board[1][0]);
+    EXPECT_EQ(5, m_board[1][1]);
     
-    EXPECT_EQ(1, my_board[0][0]);
-    EXPECT_EQ(6, my_board[1][0]);
-    EXPECT_EQ(8, my_board[1][1]);
-    
-    my_board[1][1] = 9;
-    EXPECT_EQ(9, my_board[1][1]);
+    m_board[1][1] = 9;
+    EXPECT_EQ(9, m_board[1][1]);
 }
